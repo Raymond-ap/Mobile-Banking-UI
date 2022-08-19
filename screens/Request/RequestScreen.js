b33b18c2f253b07dc9955c70117fd1c0e9122e82
Apungu Raymond <apunguray@gmail.com>
2022-08-20 01:08:20 +0800

    initial commit

---
+++ screens/Request/RequestScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RequestScreen() {
+  return (
+    <View>
+      <Text>RequestScreen</Text>
+    </View>
+  )
+}
