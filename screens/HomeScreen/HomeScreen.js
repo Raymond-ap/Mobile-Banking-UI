80ae269724dd53366db75df8ed5e48b75a24aeb3
Apungu Raymond <apunguray@gmail.com>
2022-08-20 06:47:19 +0800

    local update

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import { BalanceCard } from "../../components";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1 }} className="bg-slate-100">
+      <Header />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BalanceCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const Header = () => {
+  return (
+    <View className="px-4 py-2 bg-slate-100 flex flex-row items-center justify-between">
+      <Text className="text-xl tracking-widest capitalize font-bold">Home</Text>
+      <TouchableOpacity activeOpacity={0.8}>
+        <View className="w-10 h-10 rounded-full bg-red-300" />
+      </TouchableOpacity>
+    </View>
+  );
+};
