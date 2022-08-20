55d51711224ac6b43b924442664f8499064798ff
Apungu Raymond <apunguray@gmail.com>
2022-08-20 22:32:15 +0800

    committing changes

---
+++ constants/index.js
@@ -0,0 +1,32 @@
+export const SUBSCRIPTIONS = [
+  {
+    type: "subscription",
+    list: [
+      {
+        title: "spotify",
+        img: require("../assets/images/spotify.png"),
+        amount: "$30",
+        startDate: "Nov 1, 2019",
+        endDate: "Nov 30, 2019",
+      },
+      {
+        title: "netflix",
+        img: require("../assets/images/netflix.jpg"),
+        amount: "$400",
+        startDate: "Jun 4, 2018",
+        endDate: "Feb 30, 2019",
+      },
+      {
+        title: "webflow",
+        img: require("../assets/images/webflow.png"),
+        amount: "$290",
+        startDate: "Nov 1, 2019",
+        endDate: "Nov 30, 2019",
+      },
+    ],
+  },
+  {
+    type: "spendings",
+    amount: "$100,000",
+  },
+];
