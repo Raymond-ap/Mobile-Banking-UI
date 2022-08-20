d5db34b0f169400fe23bc79e5d2e8baba83f58c8
Apungu Raymond <apunguray@gmail.com>
2022-08-20 23:42:03 +0800

    local update

--- screens/Pay/PayScreen.js
+++ screens/Pay/PayScreen.js
@@ -5,11 +5,12 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import React from "react";
 import { GlobalHeader } from "../../components";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 
 export default function PayScreen() {
   const navigation = useNavigation();
@@ -49,4 +50,20 @@ export default function PayScreen() {
       </ScrollView>
     </SafeAreaView>
   );
+}
+
+const FriendCard = ({item}) => {
+  return <TouchableOpacity activeOpacity={0.8} className="flex flex-row items-center justify-between bg-slate-700 px-3 rounded-lg">
+    <Image resizeMode="cover" 
+      source={item.img}
+      className="h-9 w-9 rounded-full"
+    />
+    <View className="mx-2" style={{flex:1}}>
+      <Text className="font-semibold text-base capitalize text-gray-300">{item.name}</Text>
+      <Text className="font-semibold text-sm capitalize">{item.handle}</Text>
+    </View>
+    <TouchableOpacity activeOpacity={1}>
+      <Ionicons name="chevron-forward" size={20} color={"#9ca3af"}/>
+    </TouchableOpacity>
+  </TouchableOpacity>
 }
