73f413e5f38992bbbd2552967fd8094301de932c
Apungu Raymond <apunguray@gmail.com>
2022-08-20 21:27:46 +0800

    Render Subscription

---
+++ components/SubscriptionHistory.js
@@ -0,0 +1,26 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import SubscriptionCard from "./SubscriptionCard";
+import {SUBSCRIPTIONS} from '../constants'
+
+
+const SubscriptionHistory = () => {
+  return (
+    <View className="my-2">
+      <Text className="mx-4 tracking-widest text-base font-bold">
+        Subscription & spendings
+      </Text>
+      <View className="mt-2 ml-3">
+        <FlatList
+          data={SUBSCRIPTIONS}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => <SubscriptionCard />}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default SubscriptionHistory;
