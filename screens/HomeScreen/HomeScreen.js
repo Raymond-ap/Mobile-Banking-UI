93e88ae2e920fc023d48a3fd243a96a13aaba007
Apungu Raymond <apunguray@gmail.com>
2022-08-20 21:27:40 +0800

    Render Subscription

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -4,13 +4,14 @@ import {
   SafeAreaView,
   TouchableOpacity,
   ScrollView,
+  Image,
 } from "react-native";
 import React from "react";
 import { BalanceCard, SubscriptionHistory } from "../../components";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={{ flex: 1 }} className="bg-slate-100">
+    <SafeAreaView style={{ flex: 1 }} className="bg-slate-50">
       <Header />
       <ScrollView showsVerticalScrollIndicator={false}>
         <BalanceCard />
@@ -22,10 +23,14 @@ export default function HomeScreen() {
 
 const Header = () => {
   return (
-    <View className="px-4 py-2 bg-slate-100 flex flex-row items-center justify-between">
+    <View className="px-4 py-2 bg-slate-50 flex flex-row items-center justify-between">
       <Text className="text-xl tracking-widest capitalize font-bold">Home</Text>
       <TouchableOpacity activeOpacity={0.8}>
-        <View className="w-10 h-10 rounded-full bg-red-300" />
+        <Image
+          resizeMode="cover"
+          source={require("../../assets/images/profile.jpg")}
+          className="w-10 h-10 rounded-full"
+        />
       </TouchableOpacity>
     </View>
   );
